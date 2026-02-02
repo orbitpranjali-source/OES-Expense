d55476274cd541cae8d2b5886027ff3dae03566a
orbitpranjali-source <orbitpranjali@gmail.com>
2026-02-02 16:54:53 +0530

    Fix: Add basename to BrowserRouter for GitHub Pages subfolder compatibility

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/OES-Expense">
         <AuthProvider>
           <Routes>
             <Route path="/" element={<Index />} />
